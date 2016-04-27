da31464f7524eae9cce66f40eddd861c3e6f9ff8
udryan10 <rgrothouse@gmail.com>
2016-04-26 22:12:23 -0400

    rubucop fixup

--- libraries/haproxy_helpers.rb
+++ libraries/haproxy_helpers.rb
@@ -11,10 +11,10 @@ module Haproxy
       spec.each do |key, value|
         return error "unknown parameter #{key}" unless config_schema.include?(key)
         # booleans are either trueclass (true) or falseclass (false). can't use is_a? for comparison
-        unless value == true || value == false
-          return error "#{key} is not of type #{value[:type]}" unless value.is_a?(config_schema[key][:type])
-        else
+        if value == true || value == false
           return error "#{key} is not a boolean" unless config_schema[key][:type] == [TrueClass, FalseClass]
+        else
+          return error "#{key} is not of type #{value[:type]}" unless value.is_a?(config_schema[key][:type])
         end
         config_schema.delete key
       end
